c661c7ea9b9565bb582e742d6b76f936af00aaae
Dion H <89318847+Inferrno26@users.noreply.github.com>
2026-05-11 19:11:09 +0200

    Finished chapters 1-9 of the course

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,65 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
-using Shortly.Client.Data.ViewModels;
-using Shortly.Data;
-using Shortly.Data.Models;
 using System.Diagnostics;
-using System.Security.Claims;
 
 namespace Shortly.Client.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
         {
-            var newUrl = new PostUrlVM();
-            return View(newUrl);
+            return View();
         }
 
-        public IActionResult ShortenUrl(PostUrlVM postUrlVM)
+        public IActionResult Privacy()
         {
-            //Validate the Model
-            if (!ModelState.IsValid)
-            {
-                return View("Index", postUrlVM);
-            }
-            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var newUrl = new Url()
-            {
-                OriginalLink = postUrlVM.Url,
-                ShortLink = GenerateShortUrl(6),
-                NrOfClicks = 0,
-                UserId = loggedInUserId,
-                DateCreated = DateTime.UtcNow
-            };
-
-            _context.Urls.Add(newUrl);
-            _context.SaveChanges();
-
-            TempData["Message"] = $"Your url was shorted successfully to {newUrl.ShortLink}";
-
-            return RedirectToAction("Index");
-        }
-
-        private string GenerateShortUrl(int length)
-        {
-            var random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            return new string(
-                Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)])
-                .ToArray()
-                );
-
+            return View();
         }
     }
 }
